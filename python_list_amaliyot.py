bd76a8041d2254f00f0c95326791c474aa3fdd2f
xolbekkk2010 <xolbekkk2010>
2025-10-23 16:23:43 +0500

    amaliyot

--- python_list_amaliyot.py
+++ python_list_amaliyot.py
@@ -1,1 +1,10 @@
-print("Assalomu alaykum")
+ism = 'abdulla','abror','islom'
+print(ism)
+xabar = 'salom abdulla yaxshimisan'
+print(xabar)
+sonlar = 2, 4, 8
+print(sonlar)
+t_shaxslar = ('Elon Mask','Bill Gates')
+print(a_shaxslar)
+z_shaxslar = ('Mirzo Ulugbek','Alisher Navoiy')
+z_shaxslar.pop['Elon Mask']
