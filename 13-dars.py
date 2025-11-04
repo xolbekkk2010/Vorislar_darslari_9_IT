bb6d337687f230d516cf7f8193fa67b42c2e5384
xolbekkk2010 <xolbekkk2010>
2025-11-04 16:29:35 +0500

    masalalar

---
+++ 13-dars.py
@@ -0,0 +1,42 @@
+# For sikl operatoriga oid masalalar
+# For1. 
+# k = int(input("k 5 ga teng"))
+# n = int(input("n 4 ga teng"))
+# for _ in range(k):
+#     print(k)
+
+# For2.
+# a = int(input("a ni kiriting"))
+# b = int(input("b ni kiriting"))
+# for _ in range(a,b):
+#     print(a,b)
+
+# For3.
+# a = int(input("a ni kiriting"))
+# b = int(input("b ni kiriting"))
+# royhat = len(range(a+1,b))
+# royhat = sorted(range(a+1,b), reverse=True)
+# for i in range(b, a):
+#     print(i)
+# print(royhat)
+
+# For4.
+#1 kg konfetning narxi
+# price_per_kg = float(input("1 kg konfetning narxini kiriting: "))
+
+# # 1 kg dan 10 kg gacha bo'lgan narxlarni chiqarish
+# for kg in range(1, 11):
+#     total_price = price_per_kg * kg
+#     print(f"{kg} kg konfet narxi: {total_price:.2f} so'm")
+# print("1 kg konfet narxi: 5000.00 so'm")
+# print("2 kg konfet narxi: 10000.00 so'm")
+# print("3 kg konfet narxi: 15000.00 so'm")
+# print("...")
+# print("10 kg konfet narxi: 50000.00 so'm")
+
+# For5.
+
+# 0.1 kg dan 1 kg gacha bo'lgan narxlarni chiqarish
+# narx = int(input("0.1 kg konfetni narxini kiriting"))
+# for i in range(0.1, 1):
+#     print(i * narx)
